cf0e4ebfd0bd9c1d657bb36f0d5baf417d0c5b20
Guilherme <guilherme.ramires@spinver.com>
2021-07-01 22:09:42 -0300

    First Commit

---
+++ README.md
@@ -0,0 +1,35 @@
+## RPG Dice
+
+Uma simples classe que imita o comportamento de dados de RPG, podendo ser utilizado para lançar um ou mais dados do tipo **D4**, **D6**, **D8**, **D10**, **D12**, **D20** e **D100** ou outros tipos de dados que o usuario quiser.
+
+A classe pode ser atribuida a uma variavel para que o dado seja lançado mais de uma vez com o método **_roll_**, tambem se pode definir o número de lados que o dado terá no construtor da classe. As informações do dado podem ser acessadas nos parâmetros **_sides_**: Quantidade de lados do dado e **_name_**: Nome do dado.
+_Exemplo_:
+
+```python
+d20 = Dice(20) # Cria um dado chamdo D20 de 20 lados.
+d20.roll() # Rola 1 vez.
+d20.roll(3) # Rola 3 vezes.
+```
+
+O retorno de **_roll_** será um dicionário python contendo uma lista das jogadas na chave "plays" e a soma dos dados na chave "result".
+
+Tambem é possivel passar um texto contendo uma instrução de rolagem no seguinte padrão "roll"+"numero de dados"+"d"+"numero de lados dos dados" para o método **_roll_by_text_** que definirá então o dado a ser usado e rolará a quantidade de vezes que foi solicitado.
+_Exemplo_:
+
+```python
+texto = 'O comando roll2d20 ira solicitar a rolagem de 2 dados D20'
+dado = Dice()
+dado.roll_by_text(texto)
+```
+
+O retorno de **_roll_by_text_** será um dicionário python contendo uma lista das jogadas na chave "plays" e a soma dos dados na chave "result". O método alterará os parâmetros **_name_** e **_sides_** para o valor do dado solicitado no texto.
+
+Sugiro utilizar diferentes váriaveis para diferentes dados e um geral para executar os textos.
+_Exemplo_:
+
+```python
+d10 = Dice(10)
+d20 = Dice(20)
+d100 = Dice(100)
+dado = Dice()
+```
---
+++ rpgdice.py
@@ -0,0 +1,64 @@
+""" 
+Created by: Guilherme J. Ramires
+Github: https://github.com/GuiJR777
+"""
+from random import randint
+import re
+
+class  Dice():
+    def __init__(self, number_of_sides:int = 6):
+        """ 
+        Define um dado com quantidade de lados escolhidos, se não definido define um D6. 
+        """
+        self.sides = int(number_of_sides)
+        self.name = f'D{self.sides}'
+
+    def roll(self, amounts:int = 1) -> dict:
+        """ 
+        Rola dado definido com quantidade de dado passada no parametro,
+        por padrão a quantidade é 1. 
+        """
+        total_result = 0
+        results = []
+        for i in range(amounts):
+            result = randint(1, self.sides)
+            print(f'[Dice {i+1} - {self.name}] -> {result} ')
+            results.append(result)
+            total_result+=result
+        if amounts > 1:
+            print(f'[Total  - {self.name}] -> {total_result} ')
+
+        return {
+            'plays' : results,
+            'total' : total_result
+        }
+
+    def roll_by_text(self, text:str) -> dict:
+        """ 
+        Localiza no texto o padrão "roll[numero de dados]d[numero de lados]", 
+        exemplo "roll2d20", e executa o comando.
+        """
+        pattern = r"(roll\d+d\d+)"
+        matches = re.findall(pattern, text)
+        if matches != []:
+            comand = str(matches[0])
+            comand = comand.replace('roll','').replace('d', ',')
+            amounts, sides = comand.split(',')
+            self.sides = int(sides)
+            self.name = f'D{self.sides}'
+
+            return self.roll(int(amounts))
+        else:
+            pass
+
+
+if __name__ == '__main__':
+    # Área de demonstrações
+    dado = Dice()
+    print(dado.name, dado.roll())
+    print(dado.name, dado.roll(3))
+
+    texto = 'Aqui vai ter um comando roll1d20 para rodar um d20'
+    resultado = dado.roll_by_text(texto)
+    print(dado.name) # O nome do dado mudou pois o ultimo utilizado foi o D20
+    print(resultado['total']) #Acesso direto a soma dos resultados
