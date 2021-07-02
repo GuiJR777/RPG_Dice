1831c496e942b56c4177861f951b804470b4c489
Guilherme <guilherme.ramires@spinver.com>
2021-07-02 17:38:28 -0300

    Commit Heroku

--- core/utils.py
+++ core/utils.py
@@ -2,7 +2,6 @@ from datetime import datetime
 from hashlib import sha256
 
 
-
 def agora():
     data_e_hora_atuais = datetime.now()
     data_e_hora_em_texto = data_e_hora_atuais.strftime('%d/%m/%Y %Hh%M')
