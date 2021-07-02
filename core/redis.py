684db68a53d94a3447e42b3010c1947e7f9444d9
Guilherme <guilherme.ramires@spinver.com>
2021-07-02 19:22:16 -0300

    Commit Heroku

--- core/redis.py
+++ core/redis.py
@@ -40,7 +40,7 @@ class Cache():
         i = 0
         for key in self.cache.keys():
             i+=1
-            response[f'{i}'] = self.cache.get(key)
+            response[f'{i}'] = self.return_db(key)
         return response
         
 
